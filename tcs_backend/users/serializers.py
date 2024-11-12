b498fc38446c68f42223333a50a5af5c6b3fa420
brunocami23 <bruno.cami@magneticash.com>
2024-11-12 20:55:43 -0300

    feature(Users): create user serializer

---
+++ tcs_backend/users/serializers.py
@@ -0,0 +1,15 @@
+# users/serilizers.py
+
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'is_admin', 'is_staff', 'is_active', 'date_joined']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
