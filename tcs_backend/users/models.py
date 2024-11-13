b4b07530a783e5b951a8e8c6a4fc41d9f3263526
brunocami23 <bruno.cami@magneticash.com>
2024-11-12 21:09:40 -0300

    Feature(Users): create users view and add url

--- tcs_backend/users/models.py
+++ tcs_backend/users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.db import models
 
 # Manager personalizado para gestionar la creación de usuarios y superusuarios
-class UsuarioManager(BaseUserManager):
+class UserManager(BaseUserManager):
     def create_user(self, email, first_name, last_name, password=None, is_staff=False, is_admin=False):
         if not email:
             raise ValueError("El usuario debe tener un correo electrónico.")
@@ -28,14 +28,31 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True, max_length=255)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    is_staff = models.BooleanField(default=False)  # Campo para verificar si es miembro del personal
-    is_admin = models.BooleanField(default=False)  # Campo para verificar si es administrador
-    is_active = models.BooleanField(default=True)  # Campo que indica si la cuenta está activa
+    is_staff = models.BooleanField(default=False)  
+    is_admin = models.BooleanField(default=False) 
+    is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_user_permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
